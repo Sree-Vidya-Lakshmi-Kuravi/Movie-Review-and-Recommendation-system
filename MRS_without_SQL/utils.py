3904e37a0b1a1240daa67598870872ba799e764a
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-10 16:03:01 +0530

    Wrote the csv utils code for utils.py

---
+++ MRS_without_SQL/utils.py
@@ -0,0 +1,45 @@
+import csv
+
+def read_csv(path):
+    with open(path, 'r', newline = '') as f:
+        reader = csv.DictReader(f)
+        data = []
+        for row in reader:
+            data.append(row)
+    return data
+        
+
+def write_row(path, data):
+    with open(path, 'a', newline = '') as f:
+        writer = csv.DictWriter(f, fieldnames = get_headers(filename))
+        writer.writerow(data)
+
+def write_rows(filename, data):
+    headers = get_headers(filename)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames = headers)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def get_headers(filename):
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        return reader.fieldnames
+
+
+def generate_id(filename):
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        id = list(reader)[-1].get('id')
+        return int(id) + 1
+
+
+path = r"MRS_without_SQL\data\users.csv"
+user = {'id' : generate_id(path), 'username' : 'mdr', 'password':'mdr', 'email':'mdr@example.com'}
+users = read_csv(path)
+users.append(user)
+print(users)
+write_rows(path, users)
+
+
