74716779dd5fb5bd7232463b5d9d9adc22d682c4
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-11 17:23:48 +0530

    Added user dashboard menu in main.py

--- MRS_without_SQL/main.py
+++ MRS_without_SQL/main.py
@@ -1,19 +1,47 @@
 from utils import *
 from users import *
 
+# User Dashboard
 def dashboard(user):
-    pass
+    while True:
+        print("Select a choice(0-5):")
+        print("0. Logout\n1. Add a movie\n2. List all movies\n3. Add a review\n4. Edit a review\n5. Delete a review")
+
+        c = input("Enter the choice: ")
+        
+        if c == '0':
+            print("You have logged out successfully...")
+            exit()
+
+        elif c == '1':
+            pass
+
+        elif c == '2':
+            pass
+
+        elif c == '3':
+            pass
+
+        elif c == '4':
+            pass
+
+        elif c == '5':
+            pass
+
+        else:
+            print("Invalid Choice. Please enter your choice between 0-5")
+
 
 while True:
     print("----------- WELCOME TO MOVIE REVIEW SYSTEM -----------")
     print("0. Exit\n1. Register\n2. Login")
 
-    ch = int(input("Please enter a choice (0/1/2): "))
+    ch = input("Please enter a choice (0/1/2): ")
 
-    if ch == 0:
+    if ch == '0':
         break
 
-    elif ch == 1:
+    elif ch == '1':
         username = input("Enter the username: ")
         password = input("Enter the password: ")
         email = input("Enter the email: ")
@@ -21,7 +49,7 @@ while True:
         register_user = register(username, password, email)
         print("User has been registered successfully..")
 
-    elif ch == 2:
+    elif ch == '2':
         print("Enter your details to login")
         credential = input("Enter your credential(username or email): ")
         log_password = input("Enter your password: ")
