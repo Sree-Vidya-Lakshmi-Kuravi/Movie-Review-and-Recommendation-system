d458f2f1cbcebf09998897e502d087d12d71eda6
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-11 12:10:23 +0530

    Changes the code for better understanding

--- MRS_without_SQL/utils.py
+++ MRS_without_SQL/utils.py
@@ -11,11 +11,11 @@ def read_csv(path):
 
 def write_row(path, data):
     with open(path, 'a', newline = '') as f:
-        writer = csv.DictWriter(f, fieldnames = get_headers(filename))
+        writer = csv.DictWriter(f, fieldnames = get_headers(path))
         writer.writerow(data)
 
 def write_rows(filename, data):
-    headers = get_headers(filename)
+    headers = get_headers(path)
     with open(filename, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames = headers)
         writer.writeheader()
@@ -36,7 +36,7 @@ def generate_id(filename):
 
 
 path = r"MRS_without_SQL\data\users.csv"
-user = {'id' : generate_id(path), 'username' : 'mdr', 'password':'mdr', 'email':'mdr@example.com'}
+user = {'id' : generate_id(path), 'username' : 'anirudh', 'password':'ani', 'email':'ani.r@example.com'}
 users = read_csv(path)
 users.append(user)
 print(users)
