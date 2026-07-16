969bef5c754e15e8ccb532c80ac472a5993d0d69
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-16 16:40:33 +0530

    Added view_user_reviews function with body

--- MRS_without_SQL/movie_reviews.py
+++ MRS_without_SQL/movie_reviews.py
@@ -1,4 +1,12 @@
+from movies import *
+from reviews import *
+
 class MovieReviewSystem:
     def __init__(self, user):
         self.user = user
 
+    def add_movie(self, movie_title, movie_genre, movie_release_year):
+        add_movie(movie_title, movie_genre, movie_release_year)
+
+    def view_my_reviews(self):
+        get_user_review(self.user.get('id'))
