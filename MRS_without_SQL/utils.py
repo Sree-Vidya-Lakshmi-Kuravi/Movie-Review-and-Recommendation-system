2d8d8ee5d254c66904079401eeb8ca3302a66160
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-17 19:30:56 +0530

    Renamed variables for consistency

--- MRS_without_SQL/utils.py
+++ MRS_without_SQL/utils.py
@@ -1,38 +1,34 @@
 import csv
 
-def read_csv(path):
-    with open(path, 'r', newline = '') as f:
-        reader = csv.DictReader(f)
-        data = []
-        for row in reader:
-            data.append(row)
-    return data
-        
-
-def write_row(path, data):
-    with open(path, 'a', newline = '') as f:
-        writer = csv.DictWriter(f, fieldnames = get_headers(path))
-        writer.writerow(data)
-
-def write_rows(filename, data):
-    headers = get_headers(path)
-    with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames = headers)
-        writer.writeheader()
-        writer.writerows(data)
-
-
 def get_headers(filename):
     with open(filename) as f:
         reader = csv.DictReader(f)
         return reader.fieldnames
 
-
 def generate_id(filename):
     with open(filename) as f:
         reader = csv.DictReader(f)
         id = list(reader)[-1].get('id')
-        return int(id) + 1
+        return str(int(id) + 1)
+
+def read_csv(filename):
+    with open(filename) as f:
+        csv_data = csv.DictReader(f)
+        return list(csv_data)
+
+def write_row(filename, data):
+    with open(filename, 'a', newline = '') as f:
+        writer = csv.DictWriter(f, fieldnames = get_headers(filename))
+        writer.writerow(data)
+        return "Single row has been added successfully"
+
+def write_rows(filename, data):
+    headers = get_headers(filename)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames = headers)
+        writer.writeheader()
+        writer.writerows(data)
+        return "Multiple rows has been added successfully"
 
 
 # path = r"MRS_without_SQL\data\users.csv"
