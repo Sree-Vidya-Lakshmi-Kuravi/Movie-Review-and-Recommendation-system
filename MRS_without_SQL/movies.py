878394946aa102c0cea951e8df78b77beeef73a0
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-17 19:51:03 +0530

    Optimized the code for better understandability using movies.py

--- MRS_without_SQL/movies.py
+++ MRS_without_SQL/movies.py
@@ -1,44 +1,40 @@
 from utils import *
 
-path = r'MRS_without_SQL\data\movies.csv'
-def view_all_movies():
-    movie_data = read_csv(path)
+movies_file = r'MRS_without_SQL\data\movies.csv'
 
+def view_all_movies():
+    movie_data = read_csv(movies_file)
     # Handling empty data
     if not movie_data:
-        print("No movies found")
+        return "No movies found"
 
     print(f"{'Movie ID':<10} {'Movie Title':<50} {'Movie Genre':<20} {'Release Year':<15}")
     print("-" * 90)
     for m in movie_data:
         print(f"{m['id']:<10} {m['title']:<50} {m['genre']:<20} {m['release_year']:<15}")
 
-
 def add_movie(movie_title, movie_genre, movie_release_year):
-    movies = read_csv(path)
+    movie_data = read_csv(movies_file)
     new_movie = {
-        'id': generate_id(path),
+        'id': generate_id(movies_file),
         'title': movie_title,
         'genre': movie_genre,
         'release_year': movie_release_year
     }
-    write_row(path, new_movie)
+    write_row(movies_file, new_movie)
 
-
-def get_movie_by_id(movie_id: str):
-    movies = read_csv(path)
-    for m in movies:
+def get_movie_by_id(movies_file, movie_id):
+    movie_data = read_csv(movies_file)
+    for m in movie_data:
         if m['id'] == movie_id:
             return m
     return "Movie not found"
+# print(get_movie_by_id(movies_file, '2'))
 
-# print(get_movie_by_id('3'))
-
-def generate_movie_id(movie_title: str):
-    movies = read_csv(path)
-    for m in movies:
+def get_movie_by_title(movies_file, movie_title):
+    movie_data = read_csv(movies_file)
+    for m in movie_data:
         if m['title'].lower().strip() == movie_title.lower().strip():
-            return m['id']
+            return m 
     return "Movie not found"
-
-print(generate_movie_id('godavari'))
+# print(get_movie_by_title(movies_file, 'godavari'))
