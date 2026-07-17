22c5729faa9290eb434ad2a93b0e14c498a05b4e
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-17 20:14:09 +0530

    Added view_user_review() in reviews.py

--- MRS_without_SQL/reviews.py
+++ MRS_without_SQL/reviews.py
@@ -1,28 +1,33 @@
 from utils import *
+from movies import *
 
-path = r'MRS_without_SQL\data\reviews.csv'
-
-def get_user_review(user_id):
-    reviews = read_csv(path)
-    user_review_li = []
-    for r in reviews:
-        if r['user_id'] == user_id:
-            user_review_li.append(r) 
-    
-    return user_review_li
-
-# print(get_user_review('2'))
-
+reviews_file = r'MRS_without_SQL\data\reviews.csv'
+movies_file = r'MRS_without_SQL\data\movies.csv'
 
 def add_review(u_id, m_id, m_rating, m_comment):
-    reviews = read_csv(path)
+    reviews = read_csv(reviews_file)
+    movie = get_movie_by_id(movies_file, m_id)
+    if movie == "Movie not found":
+        return "Movie is not found. So, unable to write a review"
 
     new_review = {
-        'id': generate_id(path),
+        'id': generate_id(reviews_file),
         'user_id': u_id,
         'movie_id': m_id,
         'rating': m_rating,
         'comment': m_comment
     }
+    write_row(reviews_file, new_review)
 
-    write_row(path, new_review)
+def view_user_review(u_id):
+    reviews = read_csv(reviews_file)
+    found = False
+    for r in reviews:
+        if r['user_id'] == u_id:
+            print("User ID:", r['user_id'], "||", "Movie ID:", r['movie_id'], "||", "Movie Rating:", r['rating'], "||", "Movie Comment: ", r['comment'])
+            found = True
+
+    if not found:
+        print(f"No reviews found with user ID {u_id}")
+
+# view_user_review('3')
